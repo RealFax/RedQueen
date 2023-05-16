2a3531eac42a8a9b472310d3620a8cf6926deee4
PotatoCloud <xiaofeng@tzai.net>
2023-05-16 17:17:28 +0800

    clear code

--- _bin/RedQueenServer/main.go
+++ _bin/RedQueenServer/main.go
@@ -1,15 +1,3 @@
 package main
 
-import (
-	"github.com/RealFax/RedQueen/config"
-	"log"
-	"os"
-)
-
-func main() {
-	args, err := config.ReadFromArgs(os.Args...)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(args)
-}
+func main() {}
