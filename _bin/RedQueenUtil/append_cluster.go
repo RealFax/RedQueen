6dbc3bf930e540a600b6045f2bdcd3276cee0eb7
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2023-05-31 15:24:43 +0800

    add RedQueenUtil, update client conn

---
+++ _bin/RedQueenUtil/append_cluster.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"github.com/RealFax/RedQueen/client"
+	"time"
+)
+
+type appendCluster struct {
+	cfg appendClusterConfig
+}
+
+func (c *appendCluster) configEntity() config {
+	return &c.cfg
+}
+
+func (c *appendCluster) exec() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	call, err := client.New(ctx, endpoints, true)
+	if err != nil {
+		return err
+	}
+
+	return call.AppendCluster(ctx, c.cfg.serverID, c.cfg.peerAddr, c.cfg.voter)
+}
+
+func newAppendCluster() *appendCluster {
+	return &appendCluster{}
+}
--- _bin/RedQueenUtil/main.go
+++ _bin/RedQueenUtil/main.go
@@ -1,3 +1,75 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const metadataFile string = "./.r_metadata"
+
+var (
+	enableRemote bool
+	endpoints    []string
+)
+
+func init() {
+	f, err := os.Open(metadataFile)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	var metadata GenerateMetadataConfig
+	if err = json.NewDecoder(f).Decode(&metadata); err != nil {
+		return
+	}
+
+	endpoints = strings.Split(metadata.Endpoints, ",")
+	if len(endpoints) == 0 {
+		fmt.Println("parse endpoints error")
+		os.Exit(0)
+	}
+
+	enableRemote = true
+}
+
+type handler interface {
+	configEntity() config
+	exec() error
+}
+
+func main() {
+	m := map[string]handler{
+		"generate-metadata": newGenerateMetadata(),
+		"append-cluster":    newAppendCluster(),
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Print(usage)
+		fmt.Println("error: no subcommand provided")
+		return
+	}
+
+	handle, ok := m[os.Args[1]]
+	if !ok {
+		fmt.Print(usage)
+		fmt.Println("error: unmatched subcommand handler")
+		return
+	}
+
+	cfg := handle.configEntity()
+
+	if err := readFromArgs(cfg, os.Args...); err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	if err := handle.exec(); err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	fmt.Println("success")
+}
