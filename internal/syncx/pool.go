f838c17dd3524fa58c83d1535e3483e43ec4a6a8
PotatoCloud <xiaofeng@tzai.net>
2023-06-03 10:35:11 +0800

    Change the storage method of the raft server

---
+++ internal/syncx/pool.go
@@ -0,0 +1,64 @@
+package syncx
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type Pool[T any] struct {
+	pool    sync.Pool
+	onAlloc func(val T)
+	onFree  func(val T)
+}
+
+type PoolVarWrap[T any] struct {
+	freed atomic.Bool
+	pool  *Pool[T]
+	val   T
+}
+
+func (pv *PoolVarWrap[T]) Free() {
+	if pv.freed.Load() {
+		panic("value has freed")
+	}
+	pv.pool.Free(pv.val)
+	pv.freed.Swap(true)
+}
+
+func (pv *PoolVarWrap[T]) Val() T {
+	if pv.freed.Load() {
+		panic("value has freed")
+	}
+	return pv.val
+}
+
+func (p *Pool[T]) Alloc() *PoolVarWrap[T] {
+	val := p.pool.Get().(T)
+	if p.onAlloc != nil {
+		p.onAlloc(val)
+	}
+
+	return &PoolVarWrap[T]{
+		pool: p,
+		val:  val,
+	}
+}
+
+func (p *Pool[T]) Free(val T) {
+	if p.onFree != nil {
+		p.onFree(val)
+	}
+	p.pool.Put(val)
+}
+
+func NewPool[T any](new func() T, onAlloc func(val T), onFree func(val T)) *Pool[T] {
+	return &Pool[T]{
+		pool: sync.Pool{
+			New: func() any {
+				return new()
+			},
+		},
+		onAlloc: onAlloc,
+		onFree:  onFree,
+	}
+}
--- server_util.go
+++ server_util.go
@@ -5,9 +5,10 @@ import (
 	"github.com/RealFax/RedQueen/config"
 	"github.com/RealFax/RedQueen/store"
 	"github.com/RealFax/RedQueen/store/nuts"
+	"path/filepath"
 )
 
-func newNutsStore(cfg config.Store) (store.Store, error) {
+func newNutsStore(cfg config.Store, dir string) (store.Store, error) {
 	if cfg.Nuts.StrictMode {
 		nuts.EnableStrictMode()
 	} else {
@@ -17,16 +18,34 @@ func newNutsStore(cfg config.Store) (store.Store, error) {
 	return nuts.New(nuts.Config{
 		NodeNum: cfg.Nuts.NodeNum,
 		Sync:    cfg.Nuts.Sync,
-		DataDir: cfg.Nuts.DataDir,
+		DataDir: filepath.Join(dir, "fsm"),
 	})
 }
 
-func newStoreBackend(cfg config.Store) (store.Store, error) {
-	handle, ok := map[config.EnumStoreBackend]func(config.Store) (store.Store, error){
+func newStoreBackend(cfg config.Store, dir string) (store.Store, error) {
+	handle, ok := map[config.EnumStoreBackend]func(config.Store, string) (store.Store, error){
 		config.StoreBackendNuts: newNutsStore,
 	}[cfg.Backend]
 	if !ok {
 		return nil, errors.New("unsupported store backend")
 	}
-	return handle(cfg)
+	return handle(cfg, dir)
 }
+
+//func loadTLSConfig(certFile, keyFile string) (*tls.Config, error) {
+//	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return &tls.Config{
+//		Certificates:     []tls.Certificate{cert},
+//		MinVersion:       tls.VersionTLS12,
+//		CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+//		CipherSuites: []uint16{
+//			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+//			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+//			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+//			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+//		},
+//	}, nil
+//}
