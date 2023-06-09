74ac58f899c5e88f646736c33459d027694fb634
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2023-06-09 22:06:34 +0800

    [Server::clean code & new raft wrapper]

--- raft.go
+++ raft.go
@@ -5,31 +5,15 @@ import (
 	"github.com/hashicorp/raft"
 	raftboltdb "github.com/hashicorp/raft-boltdb/v2"
 	"github.com/pkg/errors"
+	"io"
 	"net"
 	"os"
 	"path/filepath"
 	"time"
 )
 
-type Command int32
-
-const (
-	SetWithTTL Command = iota
-	TrySetWithTTL
-	Set
-	TrySet
-	Del
-)
-
-type LogPayload struct {
-	Command   Command `json:"command" msgpack:"cmd"`
-	TTL       *uint32 `json:"ttl,omitempty" msgpack:"ttl,omitempty"`
-	Namespace *string `json:"namespace,omitempty" msgpack:"ns,omitempty"`
-	Key       []byte  `json:"key,omitempty" msgpack:"k,omitempty"`
-	Value     []byte  `json:"value,omitempty" msgpack:"v,omitempty"`
-}
-
 type RaftConfig struct {
+	Bootstrap               bool
 	MaxSnapshots            int
 	ServerID, Addr, DataDir string
 	Store                   store.Store
@@ -37,6 +21,15 @@ type RaftConfig struct {
 }
 
 type Raft struct {
+	bootstrap bool
+	clusters  []raft.Server
+
+	cfg           *raft.Config
+	fsm           raft.FSM
+	logStore      raft.LogStore
+	stableStore   raft.StableStore
+	snapshotStore raft.SnapshotStore
+	transport     raft.Transport
 	*raft.Raft
 }
 
@@ -44,49 +37,123 @@ func (r *Raft) AddCluster(id raft.ServerID, addr raft.ServerAddress) error {
 	return r.AddVoter(id, addr, 0, 0).Error()
 }
 
-func NewRaft(bootstrap bool, cfg RaftConfig) (*Raft, error) {
-	raftCfg := raft.DefaultConfig()
-	raftCfg.LocalID = raft.ServerID(cfg.ServerID)
+type RaftServerOption func(*Raft) error
 
-	raftCfg.LogLevel = "INFO"
+func RaftWithEmpty() RaftServerOption { return func(r *Raft) error { return nil } }
 
-	logStore, err := raftboltdb.NewBoltStore(filepath.Join(cfg.DataDir, "raft-log.db"))
-	if err != nil {
-		return nil, errors.Wrap(err, "raft-log")
+func RaftWithBootstrap() RaftServerOption {
+	return func(r *Raft) error {
+		r.bootstrap = true
+		return nil
 	}
+}
 
-	stableStore, err := NewStableStore(cfg.Store)
-	if err != nil {
-		return nil, errors.Wrap(err, "raft-stable-store")
+func RaftWithClusters(clusters []raft.Server) RaftServerOption {
+	return func(r *Raft) error {
+		r.clusters = clusters
+		return nil
 	}
+}
 
-	snapshot, err := raft.NewFileSnapshotStore(cfg.DataDir, cfg.MaxSnapshots, os.Stderr)
-	if err != nil {
-		return nil, errors.Wrap(err, "snapshot")
+func RaftWithConfig(cfg *raft.Config) RaftServerOption {
+	return func(r *Raft) error {
+		r.cfg = cfg
+		return nil
 	}
+}
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", cfg.Addr)
-	if err != nil {
-		return nil, errors.Wrap(err, "resolve-tcp-addr")
+func RaftWithStdFSM(store store.Store) RaftServerOption {
+	return func(r *Raft) error {
+		r.fsm = NewFSM(store)
+		return nil
 	}
+}
 
-	transport, err := raft.NewTCPTransport(cfg.Addr, tcpAddr, 10, time.Second*5, os.Stderr)
-	if err != nil {
-		return nil, errors.Wrap(err, "tcp-transport")
+func RaftWithBoltLogStore(path string) RaftServerOption {
+	return func(r *Raft) (err error) {
+		r.logStore, err = raftboltdb.NewBoltStore(path)
+		if err != nil {
+			return errors.Wrap(err, "bolt-log-store")
+		}
+		return
 	}
+}
 
-	r, err := raft.NewRaft(raftCfg, NewFSM(cfg.Store), logStore, stableStore, snapshot, transport)
-	if err != nil {
-		return nil, errors.Wrap(err, "raft")
+func RaftWithStdStableStore(store store.Store) RaftServerOption {
+	return func(r *Raft) (err error) {
+		r.stableStore, err = NewStableStore(store)
+		if err != nil {
+			return errors.Wrap(err, "std-stable-store")
+		}
+		return
 	}
+}
 
-	if bootstrap {
-		if err = r.BootstrapCluster(raft.Configuration{
-			Servers: cfg.Clusters,
-		}).Error(); err != nil {
+func RaftWithFileSnapshotStore(path string, retain int, logOut io.Writer) RaftServerOption {
+	return func(r *Raft) (err error) {
+		r.snapshotStore, err = raft.NewFileSnapshotStore(path, retain, logOut)
+		if err != nil {
+			return errors.Wrap(err, "file-snapshot-store")
+		}
+		return
+	}
+}
+
+func RaftWithTCPTransport(addr string, maxPool int, timeout time.Duration, logOut io.Writer) RaftServerOption {
+	return func(r *Raft) error {
+		tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+		if err != nil {
+			return errors.Wrap(err, "resolve-tcp-addr")
+		}
+		if r.transport, err = raft.NewTCPTransport(addr, tcpAddr, maxPool, timeout, logOut); err != nil {
+			return errors.Wrap(err, "tcp-transport")
+		}
+		return nil
+	}
+}
+
+func NewRaftWithOptions(opts ...RaftServerOption) (*Raft, error) {
+	var (
+		err error
+		r   = &Raft{}
+	)
+
+	for _, opt := range opts {
+		if err = opt(r); err != nil {
 			return nil, err
 		}
 	}
 
-	return &Raft{r}, nil
+	if r.bootstrap {
+		if fErr := r.BootstrapCluster(raft.Configuration{
+			Servers: r.clusters,
+		}); fErr != nil {
+			return nil, fErr.Error()
+		}
+	}
+
+	return r, nil
+}
+
+func NewRaft(cfg RaftConfig) (*Raft, error) {
+	return NewRaftWithOptions(
+		RaftWithClusters(cfg.Clusters),
+		RaftWithConfig(func() *raft.Config {
+			raftCfg := raft.DefaultConfig()
+			raftCfg.LocalID = raft.ServerID(cfg.ServerID)
+			raftCfg.LogLevel = "INFO"
+			return raftCfg
+		}()),
+		RaftWithStdFSM(cfg.Store),
+		RaftWithBoltLogStore(filepath.Join(cfg.DataDir, "raft-log.db")),
+		RaftWithStdStableStore(cfg.Store),
+		RaftWithFileSnapshotStore(cfg.DataDir, cfg.MaxSnapshots, os.Stderr),
+		RaftWithTCPTransport(cfg.Addr, 32, time.Second*3, os.Stderr),
+		func() RaftServerOption {
+			if cfg.Bootstrap {
+				return RaftWithBootstrap()
+			}
+			return RaftWithEmpty()
+		}(),
+	)
 }
