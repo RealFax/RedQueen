cf66b8ca42110d947056b127f441189a1e823343
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2023-06-24 17:20:20 +0800

    [Server::issue#33 Multi-request merging impl (basic)]

---
+++ internal/collapsar/writer.go
@@ -0,0 +1,73 @@
+package collapsar
+
+import (
+	"encoding/gob"
+	"github.com/pkg/errors"
+	"io"
+	"sync"
+	"sync/atomic"
+)
+
+type writer struct {
+	state     uint32
+	max, size int32
+	onFull    chan struct{}
+	mu        sync.Mutex
+	entries   [][]byte
+}
+
+func (a *writer) onFullTrigger() {
+	if atomic.LoadUint32(&a.state) == 0 || atomic.LoadInt32(&a.max) != atomic.LoadInt32(&a.size) {
+		return
+	}
+	_ = a.Close()
+}
+
+func (a *writer) Encode(w io.Writer) error {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	return gob.NewEncoder(w).Encode(Binary{
+		Size:    atomic.LoadInt32(&a.size),
+		Entries: a.entries,
+	})
+}
+
+func (a *writer) Add(entry []byte) error {
+	if atomic.LoadInt32(&a.max) < atomic.LoadInt32(&a.size) {
+		return errors.New("reach the max")
+	}
+	// put to entries
+	a.entries[atomic.AddInt32(&a.size, 1)-1] = entry
+	a.onFullTrigger()
+	return nil
+}
+
+func (a *writer) Wait() {
+	if atomic.LoadUint32(&a.state) == 0 {
+		return
+	}
+	<-a.onFull
+}
+
+func (a *writer) Close() error {
+	if atomic.LoadUint32(&a.state) == 0 {
+		return errors.New("repeat close")
+	}
+	atomic.StoreUint32(&a.state, 0)
+
+	a.mu.Lock()
+	close(a.onFull)
+	a.mu.Unlock()
+
+	return nil
+}
+
+func NewWriter(max int32) Writer {
+	return &writer{
+		state:   1,
+		max:     max,
+		size:    0,
+		onFull:  make(chan struct{}),
+		entries: make([][]byte, max),
+	}
+}
---
+++ internal/collapsar/writer_test.go
@@ -0,0 +1,49 @@
+package collapsar_test
+
+import (
+	"bytes"
+	"github.com/RealFax/RedQueen/internal/collapsar"
+	"testing"
+)
+
+func TestWriter_Encode(t *testing.T) {
+	w := collapsar.NewWriter(1)
+	if err := w.Add([]byte{}); err != nil {
+		t.Fatal(err)
+	}
+
+	buf := &bytes.Buffer{}
+	if err := w.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("[+] Encoding size: %d", buf.Len())
+}
+
+func TestWriter_Add(t *testing.T) {
+	w := collapsar.NewWriter(1)
+	if err := w.Add([]byte{}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestWriter_Wait(t *testing.T) {
+	w := collapsar.NewWriter(4)
+
+	go func() {
+		for i := 0; i < 4; i++ {
+			w.Add([]byte{byte(i)})
+			t.Logf("[+] Add: %d", i+1)
+		}
+	}()
+
+	w.Wait()
+	t.Log("[+] Done")
+}
+
+func BenchmarkWriter_Add(b *testing.B) {
+	w := collapsar.NewWriter(int32(b.N))
+	for i := 0; i < b.N; i++ {
+		w.Add([]byte{byte(i)})
+	}
+}
