fb038268aa1782c78e284c9c1a740e202d4fb07c
PotatoCloud <xiaofeng@tzai.net>
2023-06-29 09:14:38 +0800

    [Server::add some feature to `raft_log_apply.go`]

--- raft_log_packer.go
+++ raft_log_packer.go
@@ -3,6 +3,7 @@ package red
 import (
 	"encoding/binary"
 	"github.com/RealFax/RedQueen/api/serverpb"
+	"github.com/RealFax/RedQueen/internal/collapsar"
 	"github.com/pkg/errors"
 	"google.golang.org/protobuf/proto"
 	"io"
@@ -19,7 +20,13 @@ func putLogPackHeader(w io.Writer, typ uint32) {
 	w.Write(p)
 }
 
-func NewSingleLogPack(w io.Writer, m *serverpb.RaftLogPayload) error {
+func getLogPackHeader(r io.Reader) uint32 {
+	p := make([]byte, 4)
+	r.Read(p)
+	return binary.LittleEndian.Uint32(p)
+}
+
+func PackSingleLog(w io.Writer, m *serverpb.RaftLogPayload) error {
 	putLogPackHeader(w, SingleLogPack)
 	cmd, err := proto.Marshal(m)
 	if err != nil {
@@ -27,4 +34,46 @@ func NewSingleLogPack(w io.Writer, m *serverpb.RaftLogPayload) error {
 	}
 	w.Write(cmd)
 	return nil
+}
+
+func UnpackSingleLog(r io.Reader) (*serverpb.RaftLogPayload, error) {
+	if getLogPackHeader(r) != SingleLogPack {
+		return nil, errors.New("invalid single log header")
+	}
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	m := &serverpb.RaftLogPayload{}
+	if err = proto.Unmarshal(b, m); err != nil {
+		return nil, errors.Wrap(err, "unmarshal raft log error")
+	}
+	return m, nil
+}
+
+func UnpackMultipleLog(r io.Reader) ([]*serverpb.RaftLogPayload, error) {
+	if getLogPackHeader(r) != MultipleLogPack {
+		return nil, errors.New("invalid multiple log header")
+	}
+	cr, err := collapsar.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		logs []*serverpb.RaftLogPayload
+		m    *serverpb.RaftLogPayload
+	)
+	for {
+		b, rErr := cr.Next()
+		if rErr != nil {
+			if rErr == io.EOF {
+				return logs, nil
+			}
+			return nil, err
+		}
+		if err = proto.Unmarshal(b, m); err != nil {
+			return nil, errors.Wrap(err, "unmarshal raft log error")
+		}
+		logs = append(logs, m)
+	}
 }
