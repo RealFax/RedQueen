b565e63150051ec873265a662b00241883a36792
S13Gitee <nil@realfax.org>
2023-07-22 17:53:00 +0800

    [Server::fixed buffer allocation performance issues and improve the implementation of merging multiple requests]

--- raft_fsm.go
+++ raft_fsm.go
@@ -12,86 +12,9 @@ import (
 )
 
 type FSMHandleFunc func(*serverpb.RaftLogPayload) error
-
 type FSM struct {
-	handles map[serverpb.RaftLogCommand]FSMHandleFunc
-	store   store.Store
-}
-
-func (f *FSM) namespaceSwitch(namespace *string) (store.Namespace, error) {
-	if namespace == nil {
-		return f.store, nil
-	}
-	storeAPI, err := f.store.Namespace(*namespace)
-	if err != nil {
-		return nil, errors.Wrap(err, "namespace switch error")
-	}
-	return storeAPI, nil
-}
-
-func (f *FSM) hSetWithTTL(payload *serverpb.RaftLogPayload) error {
-	if payload.Ttl == nil || payload.Key == nil || payload.Value == nil {
-		return errors.New("invalid SetWithTTL args")
-	}
-
-	dest, err := f.namespaceSwitch(payload.Namespace)
-	if err != nil {
-		return err
-	}
-
-	return dest.SetWithTTL(payload.Key, payload.Value, *payload.Ttl)
-}
-
-func (f *FSM) hTrySetWithTTL(payload *serverpb.RaftLogPayload) error {
-	if payload.Ttl == nil || payload.Key == nil || payload.Value == nil {
-		return errors.New("invalid TrySetWithTTL args")
-	}
-
-	dest, err := f.namespaceSwitch(payload.Namespace)
-	if err != nil {
-		return err
-	}
-
-	return dest.TrySetWithTTL(payload.Key, payload.Value, *payload.Ttl)
-}
-
-func (f *FSM) hSet(payload *serverpb.RaftLogPayload) error {
-	if payload.Key == nil || payload.Value == nil {
-		return errors.New("invalid Set args")
-	}
-
-	dest, err := f.namespaceSwitch(payload.Namespace)
-	if err != nil {
-		return err
-	}
-
-	return dest.Set(payload.Key, payload.Value)
-}
-
-func (f *FSM) hTrySet(payload *serverpb.RaftLogPayload) error {
-	if payload.Key == nil || payload.Value == nil {
-		return errors.New("invalid TrySet args")
-	}
-
-	dest, err := f.namespaceSwitch(payload.Namespace)
-	if err != nil {
-		return err
-	}
-
-	return dest.TrySet(payload.Key, payload.Value)
-}
-
-func (f *FSM) hDel(payload *serverpb.RaftLogPayload) error {
-	if payload.Key == nil {
-		return errors.New("invalid Del args")
-	}
-
-	dest, err := f.namespaceSwitch(payload.Namespace)
-	if err != nil {
-		return err
-	}
-
-	return dest.Del(payload.Key)
+	handlers map[serverpb.RaftLogCommand]FSMHandleFunc
+	store    store.Store
 }
 
 func (f *FSM) Apply(log *raft.Log) any {
@@ -101,7 +24,7 @@ func (f *FSM) Apply(log *raft.Log) any {
 		if err := proto.Unmarshal(log.Data, &msg); err != nil {
 			return errors.Wrap(err, "unmarshal proto error:")
 		}
-		handle, ok := f.handles[msg.Command]
+		handle, ok := f.handlers[msg.Command]
 		if !ok {
 			return errors.New("there's no corresponding command handle")
 		}
@@ -123,16 +46,9 @@ func (f *FSM) Restore(rc io.ReadCloser) error {
 	return f.store.Restore(rc)
 }
 
-func NewFSM(s store.Store) *FSM {
-	fsm := &FSM{store: s}
-
-	fsm.handles = map[serverpb.RaftLogCommand]FSMHandleFunc{
-		serverpb.RaftLogCommand_SetWithTTL:    fsm.hSetWithTTL,
-		serverpb.RaftLogCommand_TrySetWithTTL: fsm.hTrySetWithTTL,
-		serverpb.RaftLogCommand_Set:           fsm.hSet,
-		serverpb.RaftLogCommand_TrySet:        fsm.hTrySet,
-		serverpb.RaftLogCommand_Del:           fsm.hDel,
+func NewFSM(s store.Store, handlers map[serverpb.RaftLogCommand]FSMHandleFunc) *FSM {
+	return &FSM{
+		handlers: handlers,
+		store:    s,
 	}
-
-	return fsm
 }
