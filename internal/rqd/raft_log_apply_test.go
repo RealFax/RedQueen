1f311e307e61bc83d66f6953151b88ceebb54ee4
PotatoCloud <60210021+PotatoCloud@users.noreply.github.com>
2024-01-30 14:55:18 +0800

    fix. testing file

--- internal/rqd/raft_log_apply_test.go
+++ internal/rqd/raft_log_apply_test.go
@@ -1,19 +1,9 @@
 package rqd_test
 
 import (
-	"bytes"
-	"context"
-	red "github.com/RealFax/RedQueen/internal/rqd"
-	"github.com/RealFax/RedQueen/pkg/collapsar"
-	"io"
-	"strconv"
-	"testing"
-	"time"
-
-	"github.com/hashicorp/raft"
-	"google.golang.org/protobuf/proto"
-
 	"github.com/RealFax/RedQueen/api/serverpb"
+	red "github.com/RealFax/RedQueen/internal/rqd"
+	"testing"
 )
 
 var raftLogPayloadMessage = &serverpb.RaftLogPayload{
@@ -28,56 +18,6 @@ func init() {
 
 func TestRaftLogPayloadKey(t *testing.T) {
 	t.Logf("Payload key: %d", red.RaftLogPayloadKey(raftLogPayloadMessage))
-}
-
-func TestRaftMultipleLogApply_Apply(t *testing.T) {
-	rm := red.NewRaftMultipleLogApply(
-		context.Background(),
-		3,
-		time.Millisecond*300,
-		time.Millisecond*300,
-		func(cmd []byte, timeout time.Duration) raft.ApplyFuture {
-			r, err := collapsar.NewReader(bytes.NewReader(cmd[4:]))
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			for {
-				pack, rErr := r.Next()
-				if rErr != nil {
-					if rErr == io.EOF {
-						break
-					}
-					t.Fatal(err)
-				}
-				m := &serverpb.RaftLogPayload{}
-				if err = proto.Unmarshal(pack, m); err != nil {
-					t.Fatal(err)
-				}
-				t.Log(m.String())
-			}
-			return nil
-		})
-
-	for i := 0; i < 3; i++ {
-		ctx := context.Background()
-		if err := rm.Apply(&ctx, raftLogPayloadMessage, 0); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	time.Sleep(time.Second * 1)
-
-	for i := 0; i < 3; i++ {
-		ctx := context.Background()
-		raftLogPayloadMessage.Key = []byte("test_key" + strconv.Itoa(i))
-		if err := rm.Apply(&ctx, raftLogPayloadMessage, 0); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	time.Sleep(time.Second * 1)
-
 }
 
 func BenchmarkRaftLogPayloadKey(b *testing.B) {
